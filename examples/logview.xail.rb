f4c967d1afec7f596b9ff8a7fe19e0b05a8c5b41
Wiktor Macura <wiktor@tumblr.com>
2012-02-23 22:48:29 -0500

    Modify example script: improve colors

--- examples/logview.xail.rb
+++ examples/logview.xail.rb
@@ -3,24 +3,21 @@
 # a simple log error and warning highlighter
 #
 
-class ExecuteFilter < AbstractFilter
-  def initialize(command)
-    @command = command
-  end
-
-  def streamLine(line)
-    @count ||= 0
-    @count +=1
-    puts "EXECUTING #{@count}: #{@command} #{line}"
-    return line
-  end
-end
-
-
-group("") {
-  sample 500
+#!/usr/bin/env xail
+group('fatal') {
+  contains 'fatal'
+  red
+  underscore
+  bell
 }
 
-rest {
-  stop
+group('error') {
+  contains 'error', 'exception'
+  red
+  bold
+}
+
+group('warning') {
+  contains 'warn'
+  yellow
 }
